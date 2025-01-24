2952c907ac5acf6e9285070353400ce0abfbd6bf
Seegiioo <seliga2006@gmail.com>
2025-01-24 13:46:59 +0100

    primer intento no funcional

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	public static boolean esPar(int numero) {
+        return numero % 2 == 0;
+    }
+}
